c57db09a5afa4bf8ef3edb6fd57936b827c2d4f4
BunchikovD <90457827+BunchikovD@users.noreply.github.com>
2021-10-25 12:50:38 +0300

    Add files via upload

---
+++ Pract2/Crypto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Security.Cryptography;
+//Це програма для шифрування паролів з використанням стандартних методів шифрування
+namespace Practice2
+{
+    class Crypto
+    {
+        static public byte[] GetCryptoRng(int length = 10)
+        {
+            var rngGen = new RNGCryptoServiceProvider();
+            var rndNumber = new byte[length];
+            rngGen.GetBytes(rndNumber);
+            return rndNumber;
+        }
+    }
+}
